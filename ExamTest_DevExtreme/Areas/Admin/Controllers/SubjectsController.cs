6742c9ddc8045565c3d70ffb5c27655d888ffc0c
satiando5826 <sathianpornkhamdee@gmail.com>
2019-07-08 15:22:39 +0700

    Database connect

---
+++ ExamTest_DevExtreme/Areas/Admin/Controllers/SubjectsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Exam_Test.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExamTest_DevExtreme.Areas.Admin
+{
+    [Area("Admin")]
+    public class SubjectsController : Controller
+    {
+        private readonly ExamTestDbContext _db;
+
+        public SubjectsController(ExamTestDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ExamTest_DevExtreme/Startup.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Exam_Test.DAL;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
+
+namespace ExamTest_DevExtreme
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services
+                .AddDbContext<ExamTestDbContext>()
+                .AddMvc()
+                .AddJsonOptions(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "areas",
+                    template: "{area=Admin}/{controller=Subjects}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
